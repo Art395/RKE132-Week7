be4784f435d02ee91132b9ea103e3c8d2f87aa14
Art395 <artem.chuev@tktk.ee>
2024-12-20 23:52:22 +0200

    first commit

---
+++ Degrees/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Choose the appropriate format (f/c):");
+string userChoice = Console.ReadLine();
+
+Console.WriteLine("Enter an argument:");
+int argument = Int32.Parse(Console.ReadLine());
+
+if (userChoice == "f")
+{
+    ConvertToFahrenheit(argument);
+}
+else
+{
+    ConvertToCelsius(argument);
+}
+
+
+
+static void ConvertToFahrenheit(int argument)
+{
+    Console.WriteLine($"{argument * 9 / 5 + 32}");
+}
+
+static void ConvertToCelsius(int argument)
+{
+    Console.WriteLine($"{(argument - 32) * 5 / 9}");
+}
---
+++ functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbay();
+}
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbay()
+{
+    Console.WriteLine("See you later, aligator.");
+}
